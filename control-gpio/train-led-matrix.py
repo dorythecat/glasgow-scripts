072496d51b2e1258739015b5203517d07ee7ea73
Dory <pemegomachado@gmail.com>
2025-10-16 21:18:42 +0200

    Code is now properly functional

--- control-gpio/train-led-matrix.py
+++ control-gpio/train-led-matrix.py
@@ -1,6 +1,6 @@
 """
 Code for controlling a M40057A/B (or similar) LED Matrix, used in some trains to display
-travel information.
+travel information. This will probably flicker a lot.
 
 NOTE: For the B port, do not connect the middle pin, since it's bridged to the middle in port A.
 
@@ -8,6 +8,8 @@ To run the script just run:
     glasgow script train-led-matrix.py control-gpio -V 5.0 --pins A0,A1,A2,A3,A4,A5,A6,B0,B1,B2,B3,B4,
 """
 
+import asyncio
+import random
 from glasgow.abstract import PullState
 
 # NOTE: We select a digit by doing VIO on the column and GND on the row.
@@ -26,3 +28,30 @@ await gpio_iface.output(9, True) # Column 4
 await gpio_iface.output(10, False) # Row 4
 await gpio_iface.output(11, True) # Column 1
 await gpio_iface.output(12, True) # Column 3
+await asyncio.sleep(1)
+
+rows = (8, 2, 3, 10, 5) # B2, A2, A3, B3, A5
+cols = (11, 7, 12, 4, 0, 6, 1) # B1, A0, A4, A6, B4, B5, A1, B7
+
+async def clear():
+    for row in rows:
+        await gpio_iface.set(row, True)
+    for col in cols:
+        await gpio_iface.set(col, False)
+
+async def set_pixel(x: int, y: int, on: bool) -> None:
+    if on:
+        await gpio_iface.set(cols[x], True) # Set column to VIO
+        await gpio_iface.set(rows[y], False) # Set row to GND
+    else:
+        await gpio_iface.set(cols[x], False) # Set column to GND
+        await gpio_iface.set(rows[y], True) # Set row to VIO
+
+while True:
+    await clear()
+    for _ in range(10):
+        x = random.randint(0, 6)
+        y = random.randint(0, 4)
+        await set_pixel(x, y, True)
+        await asyncio.sleep(0.05)
+        await set_pixel(x, y, False)
